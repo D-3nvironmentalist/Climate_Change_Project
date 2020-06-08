765320f06e08c24ec3374be108aa0ad11f543ad8
Stephanie <srobalino23@gmail.com>
2020-06-08 18:54:42 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// $function() {
+//     $('.for_slick_slider').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         fade: true,
+//         // cssEase: 'linear',
+//         centerMode: true,
+//         autoplaySpeed: 2000,
+//         // slidesToShow: 1,
+//         // slidesToScroll: 1
+//     });
+// }
+
+$('.single-item').slick({
+    // dots: true,
+    // centerMode: true,
+    // speed: 500,
+    // arrows: true,
+    // slidesToShow,
+    // autoplaySpeed: 2000
+});
